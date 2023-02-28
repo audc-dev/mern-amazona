57a0a7770f396e0ff8e9febf0c4d8eed72ddf966
audc-dev <cedric.audige@mail.mcgill.ca>
2023-02-28 13:36:00 -0500

    Video-08-Add-Routing

--- frontend1/src/data.js
+++ frontend1/src/data.js
@@ -1,7 +1,7 @@
 const data = {
   products: [
     {
-      name: 'Nike Slime Shirt',
+      name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/p1.jpg', //679x829
